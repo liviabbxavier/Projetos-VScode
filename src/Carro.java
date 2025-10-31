62fc425f8e67afdf531e6914679271d229b949db
unknown <liviabrocardo43@gmail.com>
2025-10-31 08:54:38 -0300

    Initial commit with project files

---
+++ src/Carro.java
@@ -0,0 +1,11 @@
+public class Carro {
+    String modelo;
+    String cor;
+    int anoDeLancamento;
+
+    public void fichaTecnica() {
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Cor: " + cor);
+        System.out.println("Ano de Lancamento: " + anoDeLancamento);
+    }
+}
---
+++ src/CarroPrincipal.java
@@ -0,0 +1,9 @@
+public class CarroPrincipal {
+    public static void main(String[] args) {
+        Carro meuCarro = new Carro();
+        meuCarro.modelo = "Opala";
+        meuCarro.cor = "Preto";
+        meuCarro.anoDeLancamento = 1968;
+        meuCarro.fichaTecnica();
+    }
+}
